095c14895be2beef2f7d40907edc23c43a914cfd
angelospk <rovas450@gmail.com>
2023-10-14 22:16:54 +0300

    mini changes

--- src/routes/trofes/+page.svelte
+++ src/routes/trofes/+page.svelte
@@ -2,19 +2,21 @@
 	import { goto } from "$app/navigation";
 	import { currentUser, pb } from "$lib/pocketbase";
     import { onMount } from "svelte";
-    let records:any[];
-    let columns:any[];
-
-onMount(async () => {
-	records = await pb.collection('feeds').getFullList({
+    let records:any[]=pb.collection('users').getFullList({
     sort: '-created',
-})
-    const fir=records[0]
-    for (let i in fir){
-        console.log(i)
-    }
+})||[];
+    // let columns:any[];
 
-})
+// onMount(async () => {
+// 	records = await pb.collection('feeds').getFullList({
+//     sort: '-created',
+// })
+//     const fir=records[0]
+//     for (let i in fir){
+//         console.log(i)
+//     }
+
+// })
 
 </script>
 {#if !$currentUser}
@@ -23,14 +25,17 @@ onMount(async () => {
 {:else}
 <h1>Οι Τροφές μου</h1>
 
-{#if records}
-<div>{JSON.stringify(records)}</div>
-
-<p class="btn variant-filled"></p>
-{:else } 
+{#await records}
 <p>loading data</p>
 
-	
-{/if}
+{:then data} 
+<div>{JSON.stringify(data)}</div>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await }
+
+
+<!-- <p class="btn variant-filled"></p> -->
+
 
 {/if}
