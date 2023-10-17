4b369b980bdf9a36581fe7ec3d3fc2422cdecf1b
angelospk <rovas450@gmail.com>
2023-10-17 21:36:14 +0300

    more changes

---
+++ src/lib/EditableTable.svelte
@@ -0,0 +1,208 @@
+<script lang="ts">
+	import FeedsTable from './FeedsTable.svelte';
+
+	import TableEditButtons from './TableEditButtons.svelte';
+
+	import { normalizeGreek } from '../greekfuncts';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+	import { metrics, feeds, userFeeds, loadedTables } from '$lib/stores/data';
+	import { currentUser, pb } from '$lib/pocketbase';
+	import type { Column, Feed, State, TableState } from '$lib/stores/types';
+	import TablePlaceHolder from '$lib/TablePlaceHolder.svelte';
+	import { getToastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
+	
+	const toastStore = getToastStore();
+	let te: ToastSettings = {
+	message: 'Δεν μπόρεσε να φορτωθεί μια τροφή (ήταν τροφή κάποιου χρήστη).',
+	timeout: 3000
+};
+	export let rationName: string="";
+	export let producerName: string="";
+
+	let certain = [
+		'Title',
+		'weight',
+		'DryMatter',
+		'Fat',
+		'CrudeFiber',
+		'CrudeProtein',
+		'Ash',
+		'Calcium',
+		'Phosphorus'
+	];
+    export let stage2Read:State={rationName, producerName, ts:{selfeeds:[],extraCols:[]}}
+	let selected:Feed[]=[];
+	let columns:Column[]=[];
+	let minimalSelected:Feed[]=[];
+	let inputChipList: string[] = [];
+	let inputChipListUser: string[] = [];
+	let inputmlist: string[] = [];
+	let autocompleteOptions: AutocompleteOption<string>[];
+	let metricsAutocomplete: AutocompleteOption<string>[];
+	let userFoodAutocomplete: AutocompleteOption<string>[];
+	export let currentState:State={rationName, producerName, ts:{selfeeds:[],extraCols:[]}}
+	$: {currentState= {rationName:rationName, producerName: producerName, ts:{selfeeds:minimalSelected, extraCols:inputmlist}};}
+// if (!waitingtoLoadState)saveState();
+
+	// reactive states updating table
+	$: columns = $metrics.filter((x) => certain.includes(x.Title) || inputmlist.includes(x.Title));
+	$: selected = [
+		...$feeds.filter((x) => inputChipList.includes(x.Title)),
+		...$userFeeds.filter((x) => inputChipListUser.includes(x.Title))
+	];
+	$: minimalSelected = selected.map((item) => {
+		if (item.id) {
+			return { id: item.id, weight: item.weight };
+		} else {
+			return { Title: item.Title, weight: item.weight };
+		}
+	});
+
+// $: {
+// 	readState(stage2Read).then((r)=>{certain=r.certain;selected=r.selected})
+// }
+
+//onmount function to initialise
+// onMount(async () => {
+	
+// 	loaded=loadState()
+// 	// waitingtoLoadState=false;
+// 	});
+
+// 	function loadState() {
+// 		const savedState = localStorage.getItem('livestockFeedState');
+// 		console.log(savedState)
+// 		if (savedState) {
+// 			return JSON.parse(savedState)
+// 			// Set other variables from the saved state if needed
+// 		}
+// 		else return null;
+// 	}
+
+
+// 	function saveState() {
+// 			localStorage.setItem('livestockFeedState', JSON.stringify(currentState));
+// 			console.log('Saved', currentState);
+// 	}
+	
+	//filtrarei ta feeds kathe fora pou allazei to inputChipList dld to koutaki pou pliktrologei o xristis
+
+
+let waitingtoLoadState=true;
+let loaded:State|null;
+async function readState(tableState:TableState) {
+    // let tableState:TableState=stage2Read?.ts
+    for (const item of tableState.selfeeds) {
+        console.log(item)
+		if (item.id && item.weight) {
+            let userFeedItem = $userFeeds.find((feed) => feed.id === item.id);
+			let itemIsByUser=true;
+			console.log(userFeedItem, $userFeeds)
+            if (!userFeedItem) {
+				itemIsByUser=false;
+                try{
+					userFeedItem = await pb.collection('feeds').getOne(item.id, {
+                    expand: 'user'
+                });
+            	}
+				catch(err){
+					console.log(err)
+					toastStore.trigger(te);
+					
+				}
+			}
+            if (userFeedItem) {
+                userFeedItem.weight = item.weight; // Update the weight
+				console.log($currentUser , itemIsByUser)
+				if (!$currentUser || !itemIsByUser) { // Assuming you have a variable `currentUser` to check if the user is signed in
+                    if (!$feeds.some(x=>x.id==userFeedItem.id)){
+					$feeds = [...$feeds, userFeedItem];} 
+					if (!inputChipList.includes(userFeedItem.Title)){
+					inputChipList.push(userFeedItem.Title);
+					}
+                }
+				else{
+				if (!inputChipListUser.includes(userFeedItem.Title)){
+					inputChipListUser.push(userFeedItem.Title);}
+
+				}
+
+
+            }
+        } else if (item.Title && item.weight) {
+            const feedItem = $feeds.find((feed) => feed.Title === item.Title);
+
+            if (feedItem) {
+                feedItem.weight = item.weight; // Update the weight
+                inputChipList.push(item.Title); // Add to inputChipList
+            }
+        }
+    }
+
+    if (tableState.extraCols && tableState.extraCols.length > 0) {
+        // certain.push(...tableState.extraCols);
+		inputmlist.push(...tableState.extraCols)
+    }
+	// console.log(inputmlist, inputChipList, inputChipListUser);
+    return { inputChipListUser, inputChipList, inputmlist };
+}
+
+$: { if($loadedTables){
+	// console.log(loaded)
+	// let t;
+	// if (loaded){
+	// 	console.log("loaded", loaded)
+	// 	t=loaded?.ts;
+	// 	producerName=loaded?.producerName
+	// 	rationName=loaded?.rationName
+	// 	if (t) readState(t).then((r)=>{inputChipList=r.inputChipList;inputChipListUser=r.inputChipListUser; inputmlist=r.inputmlist; waitingtoLoadState=false;})}
+	// }
+    let t=stage2Read?.ts
+    producerName=stage2Read?.producerName;
+    rationName=stage2Read?.rationName;
+    readState(t).then((r)=>{inputChipList=r.inputChipList;inputChipListUser=r.inputChipListUser; inputmlist=r.inputmlist; waitingtoLoadState=false;})}
+	
+}
+
+// reactive statements to change the autocomplete buttons
+	$: autocompleteOptions = $feeds.map((feed) => ({
+		label: feed.Title,
+		value: feed.Title,
+		keywords: feed.keywords
+			? feed.keywords.split(', ').concat(normalizeGreek(feed.Title))
+			: normalizeGreek(feed.Title)
+	}));
+	$: metricsAutocomplete = $metrics
+		.filter((x) => !certain.includes(x.Title))
+		.map((x) => ({
+			label: x.labelgr,
+			value: x.Title,
+			keywords: normalizeGreek(x.labelgr)
+		}));
+	$:	userFoodAutocomplete = $userFeeds.map((x) => ({
+			label: x.Title,
+			value: x.Title,
+			keywords: normalizeGreek(x.Title)
+		}));
+</script>
+
+{#if $feeds.length > 0 && $metrics.length > 0}
+<!-- selected={selected} columns={columns} -->
+<FeedsTable bind:selected={selected} bind:columns={columns} userFeeds={$userFeeds} feeds={$feeds} metrics={$metrics} edit={true} />
+{:else}
+<TablePlaceHolder></TablePlaceHolder>
+{/if}
+
+<TableEditButtons
+	currentUser={$currentUser}
+	bind:inputChipList
+	bind:inputChipListUser
+	bind:inputmlist
+	bind:autocompleteOptions
+	bind:metricsAutocomplete
+	bind:userFoodAutocomplete
+/>
+
+
+
---
+++ src/lib/TableEditButtons.svelte
@@ -0,0 +1,199 @@
+<script lang="ts">
+	import { Autocomplete } from '@skeletonlabs/skeleton';
+	import { InputChip } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+	import { popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	export let currentUser: any | null;
+//  import {certain} from './AddFoods.svelte'
+	const loginClick: PopupSettings = {
+		event: 'hover',
+		target: 'loginClick',
+		placement: 'bottom'
+	};
+	let addFoodVisible = false;
+	let addMetrics = false;
+	let addUserFoodVisible = false;
+
+	let inputChip = '';
+	export let inputChipList: string[] = [];
+	export let inputChipListUser: string[] = [];
+	let inputMetric = '';
+	export let inputmlist: string[] = [];
+	export let autocompleteOptions: AutocompleteOption<string>[];
+	export let metricsAutocomplete: AutocompleteOption<string>[];
+	export let userFoodAutocomplete: AutocompleteOption<string>[];
+
+	function userFeedsAppear() {
+		addMetrics = false;
+		addFoodVisible = false;
+		addUserFoodVisible = !addUserFoodVisible;
+	}
+	function feedAddAppear() {
+		addMetrics = false;
+		addUserFoodVisible = false;
+		addUserFoodVisible;
+		addFoodVisible = !addFoodVisible;
+	}
+	function addMetricsAppear() {
+		addFoodVisible = false;
+		addUserFoodVisible = false;
+		addMetrics = !addMetrics;
+	}
+
+	function onInputChipSelect(event: CustomEvent<AutocompleteOption<string>>): void {
+		if (inputChipList.includes(event.detail.value) === false) {
+			inputChipList = [...inputChipList, event.detail.value];
+			inputChip = '';
+		}
+	}
+	function onInputMetricSelect(event: CustomEvent<AutocompleteOption<string>>): void {
+		if (inputmlist.includes(event.detail.value) === false) {
+			inputmlist = [...inputmlist, event.detail.value];
+			inputMetric = '';
+		}
+	}
+	function onInputUserFeedSelect(event: CustomEvent<AutocompleteOption<string>>): void {
+		if (inputChipListUser.includes(event.detail.value) === false) {
+			inputChipListUser = [...inputChipListUser, event.detail.value];
+			inputChip = '';
+		}
+	}
+	function validateFoodInput(value: string): boolean {
+		if (!autocompleteOptions.map((x) => x.label).includes(value)) return false;
+		if (inputChipList.includes(value)) return false;
+		if (autocompleteOptions.filter((x) => x.keywords.includes(value))) {
+			return true;
+		}
+	}
+	function validateUserFoodInput(value: string): boolean {
+		if (!userFoodAutocomplete.map((x) => x.label).includes(value)) return false;
+		if (inputChipListUser.includes(value)) return false;
+		if (userFoodAutocomplete.filter((x) => x.keywords.includes(value))) {
+			return true;
+		}
+	}
+	function validateMetricInput(value: string): boolean {
+		// console.log(value,feeds);
+		if (!metricsAutocomplete.map((x) => x.label).includes(value)) return false;
+		if (inputmlist.includes(value)) return false;
+		if (metricsAutocomplete.map((x) => x.label).includes(value)) {
+			return true;
+		}
+	}
+	// $: autocompleteOptions = $feeds.map((feed) => ({
+	// 	label: feed.Title,
+	// 	value: feed.Title,
+	// 	keywords: feed.keywords
+	// 		? feed.keywords.split(', ').concat(normalizeGreek(feed.Title))
+	// 		: normalizeGreek(feed.Title)
+	// }));
+	// $: metricsAutocomplete = $metrics
+	// 	.filter((x) => !certain.includes(x.Title))
+	// 	.map((x) => ({
+	// 		label: x.labelgr,
+	// 		value: x.Title,
+	// 		keywords: normalizeGreek(x.labelgr)
+	// 	}));
+	// $:	userFoodAutocomplete = $userFeeds.map((x) => ({
+	// 		label: x.Title,
+	// 		value: x.Title,
+	// 		keywords: normalizeGreek(x.Title)
+	// 	}));
+</script>
+
+<div class="my-3 flex justify-between space-x-1 md:space-x-5">
+	<button class="btn variant-filled w-1/3" on:click|preventDefault={feedAddAppear}
+		>Δημόσιες Τροφές</button
+	>
+	{#if currentUser}
+		<button class="btn variant-filled w-1/3" on:click|preventDefault={userFeedsAppear}
+			>Τροφές Χρήστη</button
+		>
+	{:else}
+		<button class="btn variant-filled w-1/3" on:click|preventDefault use:popup={loginClick}
+			>Τροφές Χρήστη</button
+		>
+
+		<div class="card p-4 variant-filled-primary" data-popup="loginClick">
+			<div class="arrow variant-filled-primary" />
+			Συνδέσου για να αποθηκεύσεις και να χρησιμοποιήσεις δικές σου τροφες.
+		</div>
+	{/if}
+	<button class="btn variant-filled w-1/3" on:click|preventDefault={addMetricsAppear}>
+		Αλλαγή Στηλών</button
+	>
+</div>
+<div class="my-3 flex justify-end print:hidden">
+	{#if addMetrics}
+		<div class="card max-w-md max-h-60 p-4 overflow-y-auto" tabindex="-1">
+			<InputChip
+				bind:input={inputMetric}
+				bind:value={inputmlist}
+				name="chips"
+				validation={validateMetricInput}
+				allowUpperCase
+				placeholder="Εισάγετε στήλη..."
+			/>
+			<Autocomplete
+				bind:input={inputMetric}
+				options={metricsAutocomplete}
+				denylist={inputmlist}
+				on:selection={onInputMetricSelect}
+			/>
+		</div>
+	{/if}
+</div>
+<div class="my-3 max-w-lg flex print:hidden">
+	{#if addUserFoodVisible}
+		<div
+			class="card max-w-md mx-auto content-center justify-center max-h-60 p-4 overflow-y-auto"
+			tabindex="-1"
+		>
+			<InputChip
+				bind:input={inputChip}
+				bind:value={inputChipListUser}
+				name="στήλη"
+				validation={validateUserFoodInput}
+				allowUpperCase
+				placeholder="Εισάγετε τροφή..."
+			/>
+			<Autocomplete
+				bind:input={inputChip}
+				options={userFoodAutocomplete}
+				denylist={inputChipListUser}
+				on:selection={onInputUserFeedSelect}
+			/>
+		</div>
+	{/if}
+</div>
+<div class="my-3 max-w-lg flex justify-start print:hidden">
+	{#if addFoodVisible}
+		<div class="card max-w-md max-h-60 p-4 overflow-y-auto" tabindex="-1">
+			<InputChip
+				bind:input={inputChip}
+				bind:value={inputChipList}
+				name="στήλη"
+				validation={validateFoodInput}
+				allowUpperCase
+				placeholder="Εισάγετε τροφή..."
+			/>
+			<Autocomplete
+				bind:input={inputChip}
+				options={autocompleteOptions}
+				denylist={inputChipList}
+				on:selection={onInputChipSelect}
+			/>
+		</div>
+	{/if}
+</div>
+
+<style lang="postcss">
+
+	@media print {
+		/* Hide buttons and explanatory text */
+		.btn {
+			display: none;
+		}
+	}
+</style>
