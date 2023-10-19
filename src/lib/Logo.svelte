ce87d8c65e1d685486f5bbee9ad0550f675a6d9f
angelospk <rovas450@gmail.com>
2023-10-19 05:46:39 +0300

    new ui changes

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,7 +1,7 @@
 <script>
     export let cl="";
 </script>
-<svg version="1.0" xmlns="http://www.w3.org/2000/svg" class="w-1/12 mx-auto transition-colors duration-500 animate-bounce hover:fill-current hover:text-blue-500 "
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg" class="w-1/12 mx-auto transition-colors duration-500 animate-pulse hover:fill-current hover:text-blue-500 "
  viewBox="0 0 1024 1024"
  preserveAspectRatio="xMidYMid meet">
 
