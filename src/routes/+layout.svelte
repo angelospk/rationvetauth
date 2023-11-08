d30734e939f60a1825bf583c8482d78a2300f84c
angelospk <rovas450@gmail.com>
2023-11-08 18:22:43 +0200

    changed about

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -153,7 +153,7 @@
 										<path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
 									  </svg></button
 							>
-							{/if}
+							
 							<div class="p-2 rounded-lg variant-filled-secondary absolute text-lg top-0 left-0 z-10" data-popup="popupUser">
 								<div class="arrow variant-filled-secondary" />
 								<ol>
@@ -165,6 +165,7 @@
 									</li>
 								</ol>
 							</div>
+							{/if}
 							<!-- <Dropdown class=""  containerClass="opacity-90 relative pr-10  "
 							><DropdownItem><a href="/profile">Επεξεργασία</a></DropdownItem>
 							<DropdownItem><button on:click={logout} >Αποσύνδεση</button></DropdownItem>
