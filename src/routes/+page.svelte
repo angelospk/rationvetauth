5b65acc712990ef32b5713857cae96d214637edd
angelospk <rovas450@gmail.com>
2023-11-08 18:47:57 +0200

    change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 
-{#if $currentUser}
+<!-- {#if $currentUser}
 <h1 class="text-xl">Αρχική Σελίδα</h1>
   <p class="my-2">Καλώς ήρθες, {$currentUser.name}.<br /></p>
   <div class="flex-col w-2/3 justify-center ml-auto mr-auto flex sm:flex-row sm:space-x-5 ">
@@ -14,11 +14,17 @@
     <a href="/rations" class="my-2 koumpi">Τα Σιτηρέσιά μου</a>
     <a href="/profile" class="my-2 koumpi">Το Προφίλ μου</a>
   </div>
-{:else}
+{:else} -->
 <div class="flex-col justify-center space-y-5 lg:space-x-5 lg:flex-row flex  pt-[3vh]">
-  <div class= "text-5xl"><p class="gradient-heading">Καλώς ήλθατε!</p><br>
-    <p> Το παρόν έργο αποτελεί καίρια καινοτομία για δημιουργία - επίλυση σιτηρεσίων για τους φοιτητές αλλά και για ιδιώτες !</p></div>
+  <div class= "text-5xl "> <div class="inline-flex "><p class="gradient-heading">Καλώς ήλθατε</p>{#if $currentUser}<p class="gradient-heading ml-3">{$currentUser.name}</p>{/if}<p class="gradient-heading">!</p></div> <br>
+    <p class="my-2"> Το παρόν έργο αποτελεί καίρια καινοτομία για δημιουργία - επίλυση σιτηρεσίων για τους φοιτητές αλλά και για ιδιώτες !</p></div>
   
+    {#if $currentUser}
+    <div class=""><a href="/feeds" class="my-2 koumpi">Οι Τροφές μου</a>
+      <a href="/rations" class="my-2 koumpi">Τα Σιτηρέσιά μου</a>
+      <a href="/profile" class="my-2 koumpi">Το Προφίλ μου</a></div>
+      {:else}
   <div class="max-w-md card mx-auto"><LoginComponent /></div>
+  {/if}
 </div>
-{/if}
+<!-- {/if} -->
